67324372e24bf335a0fa4835a25fea932f5901c9
Joao Pedro Flach <joao.flach@hotmail.com>
2015-09-11 01:26:50 -0300

    Agora sim o localdb foi também. Tive que clicar em "Add Ignored to Source Control"

---
+++ TimeTracking/TimeTracking.MVC/Models/Activity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimeTracking.MVC.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+    }
+}
